7eb16fcb8679c495009c485480b8ca2caecfe9cf
Robin Cassan <robin.cassan@netc.eu>
2025-07-30 11:16:35 +0200

    fin du TP3

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,11 +1,27 @@
 package dev;
 
-/**
- * Hello world!
- */
+
+import com.github.lalyos.jfiglet.FigletFont;
+
+import java.util.ResourceBundle;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws Exception {
+//        System.out.println("Hello World!");
+//        try {
+//            String asciiArt = FigletFont.convertOneLine("Mon App");
+//            System.out.println(asciiArt);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+        String titre = ResourceBundle.getBundle("App").getString("titre");
+        String asciiArt = FigletFont.convertOneLine(titre);
+        System.out.println(asciiArt);
+
+        // affichage de l'environnement
+        String environnement = ResourceBundle.getBundle("App").getString(
+                "environnement");
+        System.out.println("Environnement : " + environnement);
 
     }
 }
